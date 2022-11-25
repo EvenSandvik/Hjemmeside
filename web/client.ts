99f2875990debac9e151e0b86c8166f9a3eb5339
Even Berge Sandvik <even.sandvik@knowit.no>
2022-11-25 13:42:29 +0100

    :)

--- web/client.ts
+++ web/client.ts
@@ -3,7 +3,7 @@
 import sanityClient from'@sanity/client'
 
 export default sanityClient({
-  projectId: 'your-project-id', // you can find this in sanity.json
+  projectId: '8itslgwn', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true // `false` if you want to ensure fresh data
 })
