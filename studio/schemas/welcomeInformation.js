3dd705cae8639f1ee0f4fb7b5063711050fe8024
Even Berge Sandvik <even.sandvik@knowit.no>
2022-11-25 13:19:57 +0100

    sanity

---
+++ studio/schemas/welcomeInformation.js
@@ -0,0 +1,18 @@
+// studio/schemas/welcomeInformation.js
+export default {
+    name: 'welcomeInformation',
+    title: 'Welcome information',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+      },
+    ],
+  }
